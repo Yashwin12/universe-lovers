adb7e55f1cf13bf2b7f4cd2d61e101eed902af9f
Yashwin12 <munsadwalayashwin@gmail.com>
2020-09-10 01:00:14 -0400

    Loads globe with resium :earth:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import { Viewer, Entity, PointGraphics, EntityDescription, KmlDataSource } from "resium";
+import { hot } from "react-hot-loader/root";
+import { Cartesian3 } from "cesium";
+  
 function App() {
   return (
-    <div>
-      <div>Hello World</div>
-    </div>
+    <Viewer full>
+      {/* <Entity
+        description="test"
+        name="tokyo"
+        point={{ pixelSize: 10 }}
+        position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
+      >
+        <EntityDescription>
+          <h1>Hello, world.</h1>
+          <p>JSX is available here!</p>
+        </EntityDescription>
+      </Entity> */}              
+      <KmlDataSource data={"http://localhost:5000/kmlFile"} />      
+      
+      {/* <KmlDataSource data={"../data/artificialSkyBrightness.kml"} />   */}
+    </Viewer>
   );
 }
 
-export default App;
+export default hot(App);
