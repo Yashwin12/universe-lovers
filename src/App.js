b5c770e9f8089691a893a831c9adeb85115d4814
Yashwin12 <munsadwalayashwin@gmail.com>
2020-09-10 23:25:13 -0400

    Separate components for Nightsky :star:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-import { Viewer, Entity, PointGraphics, EntityDescription, KmlDataSource } from "resium";
 import { hot } from "react-hot-loader/root";
-import { Cartesian3 } from "cesium";
+import NightSky from "./components/NightSky";
   
 function App() {
   return (
-    <Viewer full>
-      {/* <Entity
-        description="test"
-        name="tokyo"
-        point={{ pixelSize: 10 }}
-        position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
-      >
-        <EntityDescription>
-          <h1>Hello, world.</h1>
-          <p>JSX is available here!</p>
-        </EntityDescription>
-      </Entity> */}              
-      <KmlDataSource data={"http://localhost:5000/kmlFile"} />      
-      
-      {/* <KmlDataSource data={"../data/artificialSkyBrightness.kml"} />   */}
-    </Viewer>
+    <div>
+      <NightSky dataPath= "http://localhost:5000/kmlFile"/>
+    </div>
   );
 }
 
