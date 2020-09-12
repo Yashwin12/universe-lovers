1559384bafa47efd84934d126d058627443b1a48
Yashwin12 <munsadwalayashwin@gmail.com>
2020-09-12 01:39:31 -0400

    Corrects calling of MARS API

--- src/App.js
+++ src/App.js
@@ -5,40 +5,17 @@ import { hot } from "react-hot-loader/root";
 import NightSky from "./components/NightSky";
 import Mars from "./components/Mars";
 
-import NASAApiCall from "./utilities/NASAApiCall";
-import * as myConstClass from './utilities/Constants';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {    
-    var response = NASAApiCall( myConstClass.MARS_INSIGHT_BASE_URI, myConstClass.MARS_INSIGHT_URI_PARAMS );
-    response.then( (response) => { console.log( response); });    
-  }
-
-  render() {
-    return (
-      <div>
-        <Router>
-          <Switch>
-            <Route
-              path="/night-sky"
-              render={(routeProps) => (
-                <NightSky
-                  {...routeProps}
-                  dataPath="http://localhost:5000/kmlFile"
-                />
-              )}
-            />
-            <Route path="/mars" component={Mars} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch> 
+          <Route path = "/night-sky" render = { (routeProps) => ( <NightSky {...routeProps} dataPath= "http://localhost:5000/kmlFile"/> ) } /> 
+          <Route path = "/mars" component = {Mars}  />       
+        </Switch>        
+      </Router>                
+    </div>
+  );
 }
 
 export default hot(App);
--- src/components/Mars.js
+++ src/components/Mars.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import MarsWeatherReportGraph from "./MarsWeatherReportGraph";
 
+import NASAApiCall from "../utilities/NASAApiCall";
+import * as myConstClass from '../utilities/Constants';
+
 class Mars extends Component {
     constructor(props) {
         super(props);
         this.state = {  };
     }
+
+    componentDidMount() {    
+        var response = NASAApiCall( myConstClass.MARS_INSIGHT_BASE_URI, myConstClass.MARS_INSIGHT_URI_PARAMS );
+        response.then( (response) => { console.log( response); });    
+    }
+
     render() {
         return (
             <div>
